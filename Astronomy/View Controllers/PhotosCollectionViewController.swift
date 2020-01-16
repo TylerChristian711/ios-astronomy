faf699f8c158e0e04346a525ccba0cfb73bcafbc
Tyler Christian <tylerc71197@gmail.com>
2020-01-16 15:32:50 -0500

    parts 0, 1 and 2 finished

--- Astronomy/View Controllers/PhotosCollectionViewController.swift
+++ Astronomy/View Controllers/PhotosCollectionViewController.swift
@@ -8,66 +8,8 @@
 
 import UIKit
 
-class PhotosCollectionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+class PhotosCollectionViewController: UIViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        client.fetchMarsRover(named: "curiosity") { (rover, error) in
-            if let error = error {
-                NSLog("Error fetching info for curiosity: \(error)")
-                return
-            }
-            
-            self.roverInfo = rover
-        }
-    }
-    
-    // UICollectionViewDataSource/Delegate
-    
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return photoReferences.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageCell", for: indexPath) as? ImageCollectionViewCell ?? ImageCollectionViewCell()
-        
-        loadImage(forCell: cell, forItemAt: indexPath)
-        
-        return cell
-    }
-    
-    // Make collection view cells fill as much available width as possible
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let flowLayout = collectionViewLayout as! UICollectionViewFlowLayout
-        var totalUsableWidth = collectionView.frame.width
-        let inset = self.collectionView(collectionView, layout: collectionViewLayout, insetForSectionAt: indexPath.section)
-        totalUsableWidth -= inset.left + inset.right
-        
-        let minWidth: CGFloat = 150.0
-        let numberOfItemsInOneRow = Int(totalUsableWidth / minWidth)
-        totalUsableWidth -= CGFloat(numberOfItemsInOneRow - 1) * flowLayout.minimumInteritemSpacing
-        let width = totalUsableWidth / CGFloat(numberOfItemsInOneRow)
-        return CGSize(width: width, height: width)
-    }
-    
-    // Add margins to the left and right side
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 0, left: 10.0, bottom: 0, right: 10.0)
-    }
-    
-    // MARK: - Private
-    
-    private func loadImage(forCell cell: ImageCollectionViewCell, forItemAt indexPath: IndexPath) {
-        
-        // let photoReference = photoReferences[indexPath.item]
-        
-        // TODO: Implement image loading here
-    }
     
     // Properties
     
@@ -75,7 +17,7 @@ class PhotosCollectionViewController: UIViewController, UICollectionViewDataSour
     
     private var roverInfo: MarsRover? {
         didSet {
-            solDescription = roverInfo?.solDescriptions[3]
+            solDescription = roverInfo?.solDescriptions[105]
         }
     }
     private var solDescription: SolDescription? {
@@ -96,4 +38,79 @@ class PhotosCollectionViewController: UIViewController, UICollectionViewDataSour
     }
     
     @IBOutlet var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        client.fetchMarsRover(named: "curiosity") { (rover, error) in
+            if let error = error {
+                NSLog("Error fetching info for curiosity: \(error)")
+                return
+            }
+            
+            self.roverInfo = rover
+        }
+    }
+    
+    // UICollectionViewDataSource/Delegate
+    
+   
+    
+    // MARK: - Private
+    
+    private func loadImage(forCell cell: ImageCollectionViewCell, forItemAt indexPath: IndexPath) {
+        
+        let photoReference = photoReferences[indexPath.item]
+        guard let imageURL = photoReference.imageURL.usingHTTPS else { return }
+        URLSession.shared.dataTask(with: imageURL) { (data, _, error) in
+            if let _ = error {
+                return
+            }
+            
+            guard let data = data else { return }
+            if self.collectionView.cellForItem(at: indexPath) == cell {
+            DispatchQueue.main.async {
+                cell.imageView.image = UIImage(data: data)
+              }
+            }
+        }.resume()
+    }
+}
+
+extension PhotosCollectionViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+           return 1
+       }
+       
+       func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+           return photoReferences.count
+       }
+       
+       func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+           let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageCell", for: indexPath) as? ImageCollectionViewCell ?? ImageCollectionViewCell()
+           
+           loadImage(forCell: cell, forItemAt: indexPath)
+           
+           return cell
+       }
+       
+       // Make collection view cells fill as much available width as possible
+       func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+           let flowLayout = collectionViewLayout as! UICollectionViewFlowLayout
+           var totalUsableWidth = collectionView.frame.width
+           let inset = self.collectionView(collectionView, layout: collectionViewLayout, insetForSectionAt: indexPath.section)
+           totalUsableWidth -= inset.left + inset.right
+           
+           let minWidth: CGFloat = 150.0
+           let numberOfItemsInOneRow = Int(totalUsableWidth / minWidth)
+           totalUsableWidth -= CGFloat(numberOfItemsInOneRow - 1) * flowLayout.minimumInteritemSpacing
+           let width = totalUsableWidth / CGFloat(numberOfItemsInOneRow)
+           return CGSize(width: width, height: width)
+       }
+       
+       // Add margins to the left and right side
+       func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+           return UIEdgeInsets(top: 0, left: 10.0, bottom: 0, right: 10.0)
+       }
 }
